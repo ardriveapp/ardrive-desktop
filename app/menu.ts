73f7c0e961fb864e15585221fbd6c0ccead6dcf3
Anthony Tripaldi <anthony.tripaldi@gmail.com>
2020-08-10 21:44:44 -0400

    rerun script and update menu label

--- app/menu.ts
+++ app/menu.ts
@@ -54,7 +54,7 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'ArDrive',
       submenu: [
         {
           label: 'About ElectronReact',
