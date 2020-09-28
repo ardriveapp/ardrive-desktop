990a05c3077943f065ac870b35920880603463a0
Dmytro Baida <dmytrobaida@outlook.com>
2020-09-28 22:20:18 +0300

    removes redundant code, moves to create-react-app

--- app/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-// import routes from '../constants/routes.json';
-import styles from './Home.css';
-
-export default function Home(): JSX.Element {
-  return <div className={styles.container} data-tid="container" />;
-}
