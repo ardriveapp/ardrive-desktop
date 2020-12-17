d1414c236e87acc1d1b32fa12842f7616ddbefcd
Dmytro Baida <dmytrobaida@outlook.com>
2020-12-18 00:51:26 +0200

    adds intro info base

--- packages/renderer/src/app/pages/Welcome/Welcome.tsx
+++ packages/renderer/src/app/pages/Welcome/Welcome.tsx
@@ -7,14 +7,29 @@ import {
   WelcomeContainer,
 } from "app/components";
 import { FirstStep, SecondStep } from "./steps";
+import { useTranslationAt } from "app/utils/hooks";
 
 export default () => {
   const history = useHistory();
   const [step, setStep] = useState(0);
+  const [subStep, setSubStep] = useState(0);
+  const { t } = useTranslationAt("pages.welcome.steps");
 
   const jumpIn = useCallback(() => {
     setStep(1);
   }, []);
+
+  const goNext = useCallback(() => {
+    setSubStep((prev) => prev + 1);
+  }, []);
+
+  const goBack = useCallback(() => {
+    if (subStep === 0) {
+      setStep(0);
+      return;
+    }
+    setSubStep((prev) => prev - 1);
+  }, [subStep]);
 
   const continueHandler = useCallback(() => {
     history.push("/create-user");
@@ -25,11 +40,18 @@ export default () => {
       case 0:
         return () => <FirstStep onContinue={jumpIn} />;
       case 1:
-        return () => <SecondStep onContinue={continueHandler} />;
+        return () => (
+          <SecondStep
+            title={t(`second.substeps.${subStep}.title`)}
+            description={t(`second.substeps.${subStep}.description`)}
+            onContinue={goNext}
+            onBack={goBack}
+          />
+        );
       default:
         return React.Fragment;
     }
-  }, [step, jumpIn, continueHandler]);
+  }, [step, subStep, goNext, goBack, continueHandler]);
 
   const CurrentImage = useMemo(() => {
     switch (step) {
